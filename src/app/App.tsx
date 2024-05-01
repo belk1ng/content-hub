0869c0278206afd96b1995374a84e4f827db2e78
Dmitry Belkin <dmitriy.belkin.2001@mail.ru>
2024-05-01 11:46:29 +0300

    refactor: 💡 Extend styles with mixins, add fonts
    Extended styles with mixins. Added fonts. Added lib for BEM naming.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,16 @@ import Providers from "@/app/providers";
 import Router from "@/app/router";
 import "./styles/index.scss"
 import {useTheme} from "@/entities/theme";
+import cn from "@/shared/lib/cn"
+
+const appCn = cn("app");
 
 const App = () => {
     const {theme} = useTheme();
 
     return (
         <Providers>
-            <div className={`app app_${theme}`}>
+            <div className={appCn({[theme]: true})}>
                 <Router/>
             </div>
         </Providers>
---
+++ src/shared/lib/cn/cn.ts
@@ -0,0 +1,8 @@
+import {withNaming} from '@bem-react/classname'
+
+const cn = withNaming({
+    e: "__",
+    m: "_"
+})
+
+export default cn
